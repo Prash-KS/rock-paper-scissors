2665848bd0e465d3931c27c801e267dc8770f5fe
Prashant KS <prashantkumar39571@gmail.com>
2024-03-20 11:48:50 +0530

    Delete unnecessary js file

--- scripts.js
+++
@@ -1,9 +0,0 @@
-let buttons = document.querySelectorAll("#choice button");
-
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        e.stopPropagation();
-        console.log("Button clicked");
-        // Add your button click logic here
-    });
-});
